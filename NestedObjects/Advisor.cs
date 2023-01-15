40db3d066b9727f9a890bfe935a7b88401a37436
MistySunMew <michael.e.forbes@comcast.net>
2023-01-15 11:48:17 -0800

    Made Advisor class public

--- NestedObjects/Advisor.cs
+++ NestedObjects/Advisor.cs
@@ -9,7 +9,7 @@ namespace NestedObjects
     /// <summary>
     /// Represents a college advisor
     /// </summary>
-    internal class Advisor
+    public class Advisor
     {
         /// <summary>
         /// The legal first and last name
