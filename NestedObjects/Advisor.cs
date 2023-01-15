018d700c06a2b57f4d8746e565742f4f040bb7ca
MistySunMew <michael.e.forbes@comcast.net>
2023-01-15 11:33:40 -0800

    Created Student and Advisor classes

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a college advisor
+    /// </summary>
+    internal class Advisor
+    {
+        /// <summary>
+        /// The legal first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The work Email for the advisor
+        /// </summary>
+        public string Email { get; set; }
+
+
+        /// <summary>
+        /// Building and room number of the office
+        /// </summary>
+        public string OfficeLocation { get; set; }
+
+    }
+}
